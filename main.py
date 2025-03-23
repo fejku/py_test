2a2c496ea39308daaf6b2d426a69fad77285cdd0
fejku <przecinek45@o2.pl>
2025-03-24 00:02:24 +0100

    bind test

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import tkinter as tk
 import keyboard
+import pyautogui
 
 class Application:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("qwe2")
-
+        self.root.geometry('600x400')
+        self.root.withdraw()
         self.initHotkey()
-
         self.initDesign()
 
     def hotkey_pressed(self):
         print("Space was pressed!")
+        self.root.deiconify()
 
     def initHotkey(self):
         keyboard.add_hotkey('ctrl+shift+f1', self.hotkey_pressed)
         
     def initDesign(self):
-        self.button_one = tk.Button(self.root, text="One", command=self.onOne)
-        self.button_one.pack()
+        self.button_one = tk.Button(self.root, text="One", bg='purple', fg='white', command=self.onOne)
+        self.button_one.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+        self.button_two = tk.Button(self.root, text="Two", bg='purple', fg='white', command=self.onOne)
+        self.button_two.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+        # self.button_one.pack(pady=10)
+        # self.button_two.pack(pady=10)
+
+        self.root.bind("1", lambda event: print("test"))
+        self.root.bind("2", lambda event: print("test2"))
 
     def onOne(self):
         self.root.withdraw()
+        pyautogui.write("Q1")
+        pyautogui.press("enter")
         
 if __name__ == "__main__":
     app = Application()
