245591990cb582b818b8b898931272cd9bdc473f
fejku <przecinek45@o2.pl>
2025-03-23 12:14:21 +0100

    Dodano przycisk do minimalizowania okna

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 pip install -r requirements.txt
+pip list
+python main.py
+
+myVenv\Scripts\activate
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import tkinter as tk
 import keyboard
 
-def hotkey_pressed():
-    print("Space was pressed!")
+class Application:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("qwe2")
 
-root = tk.Tk()
-root.title("Qwe")
+        self.initHotkey()
 
-keyboard.add_hotkey('ctrl+shift+f1', hotkey_pressed)
+        self.initDesign()
 
-root.mainloop()
+    def hotkey_pressed(self):
+        print("Space was pressed!")
+
+    def initHotkey(self):
+        keyboard.add_hotkey('ctrl+shift+f1', self.hotkey_pressed)
+        
+    def initDesign(self):
+        self.button_one = tk.Button(self.root, text="One", command=self.onOne)
+        self.button_one.pack()
+
+    def onOne(self):
+        self.root.withdraw()
+        
+if __name__ == "__main__":
+    app = Application()
+    app.root.mainloop()
